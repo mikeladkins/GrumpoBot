ae5092b376be1a921612b7ba6da68d126a38e4bd
Mikel Adkins <dev.mikeladkins@gmail.com>
2021-10-14 19:29:23 -0400

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,147 @@
+import discord
+import os
+import requests
+import json
+import time
+import datetime
+import logging
+import keys
+
+client = discord.Client()
+logger = logging.getLogger('Discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+token = keys.token
+retail_v1_base_url = 'https://www.warcraftlogs.com:443/v1/'
+ffl_v1_base_url = 'https://www.fflogs.com:443/v1/'
+base_report_url = 'https://www.warcraftlogs.com/reports/'
+default_error_message = 'Something went wrong, check arguments and try again or contact Grumpo.'
+
+
+def get_character_parse_for_zone_id(game, characterName, server, region, zoneId):
+    if game == "ff" :
+        response = requests.get(
+            ffl_v1_base_url +
+            characterName + '/' + server + '/' + region + '/' + '?zone=' + zoneId +
+            '&api_key=' + keys.ffl_v1_key)
+    elif game == "retail" :
+        response = requests.get(
+            retail_v1_base_url +
+            characterName + '/' + server + '/' + region + '/' + '?zone=' + zoneId +
+            '&api_key=' + keys.retail_v1_key)
+    json_data = json.loads(response.text)
+    return json_data
+
+
+def get_most_recent_report_for_guild(guildName, server, region):
+    response = requests.get(
+        'https://www.warcraftlogs.com:443/v1/reports/guild/' + guildName +
+        '/' + server + '/' + region + '?api_key=' + keys.wcl_v1_key)
+    json_data = json.loads(response.text)
+    return json_data
+
+
+def get_reports_for_guild_by_date(guildName, server, region, startDate):
+    timeone = datetime.datetime.strptime(startDate, "%m/%d/%Y")
+    modDate = time.mktime(timeone.timetuple())
+    timestamp = modDate * 1000
+    print(timestamp)
+    response = requests.get(
+        'https://www.warcraftlogs.com:443/v1/reports/guild/' + guildName +
+        '/' + server + '/' + region + '?start=' + str(timestamp) +
+        '&api_key=' + keys.wcl_v1_key)
+    return json.loads(response.text)
+
+@client.event
+async def on_error(arg):
+    pass
+
+@client.event
+async def on_ready():
+    print('Ready to use, logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    msg = message.content
+
+    if (msg.startswith('!recent-report-for-guild')):
+        split_message = msg.split(' ', 4)
+        try:
+            guild = split_message[1]
+            server = split_message[2]
+            region = split_message[3]
+            await message.channel.send('Getting most recent report for ' +
+                                       guild + '...')
+            responseJson = get_most_recent_report_for_guild(
+                guild, server, region)
+            reportId = responseJson[0]['id']
+            output = base_report_url + reportId
+            await message.channel.send(output)
+        except Exception as e:
+            print('Error in Recent Report For Guild:\n' + str(e) + '\n')
+            await message.channel.send(default_error_message)
+
+    if msg.startswith('!report-for-guild-by-date'):
+        split_message = msg.split(' ', 4)
+        try:
+            guild = split_message[1]
+            server = split_message[2]
+            region = split_message[3]
+            dateString = split_message[4]
+            await message.channel.send('Getting report for ' + guild + ' on ' +
+                                       dateString + '...')
+            reportsJson = get_reports_for_guild_by_date(
+                guild, server, region, dateString)
+            reportId = reportsJson[-1]['id']
+            if reportId is None:
+                output = 'Could not load logs, check arguments and try again.'
+            else:
+                output = base_report_url + reportId
+                await message.channel.send(output)
+        except Exception as e:
+            print('Error in Reports for guild by date!\n' + str(e) + '\n')
+            await message.channel.send(default_error_message)
+
+    if msg.startswith('!grumpo-is-cool'):
+        await message.add_reaction('👍')
+
+    if msg.startswith('!most-recent-parse-for-character'):
+        splitMessage = msg.split(' ', 5)
+        try:
+            game = splitMessage[1]
+            character = splitMessage[2]
+            server = splitMessage[3]
+            region = splitMessage[4]
+            zoneId = str(28)  #Default to most recent raid, i.e. Sanctum of Domination
+            stringLength = len(splitMessage)
+            if stringLength == 6:
+                zoneId = splitMessage[5]
+            await message.channel.send('Getting most recent parse for ' +
+                                       character + ' in zone id: ' + zoneId +
+                                       '...')
+            report_json = get_character_parse_for_zone_id(
+                game, character, server, region, zoneId)
+            parse = report_json[0]
+            output = 'Character: ' + parse[
+                'characterName'] + '\nClass: ' + parse[
+                    'class'] + '\nSpec: ' + parse['spec'] + '\nBoss: ' + parse[
+                        'encounterName'] + '\nPercentile: ' + str(
+                            parse['percentile']) + '\nRank: ' + str(
+                                parse['rank']) + ' out of ' + str(
+                                    parse['outOf'])
+            await message.channel.send(output)
+        except Exception as e:
+            logger.error("Error in most recent parse for character:", e)
+            print('Error in Most Recent Parse for Character!\n' + str(e) +
+                  '\n')
+            await message.channel.send(default_error_message)
+
+
+#keep_alive()
+client.run(token)
